351d7469441186260cde39259f3e7f567757db2c
maybeags <suren0709@gmail.com>
2025-05-20 12:37:31 +0900

    feat: 20250520 JS review

---
+++ ch1/myapp/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <h1>Hello, React!</h1>
+      <h2>첫 번째 Component</h2>
+      <App2></App2>
+    </>
+  )
+}
+
+function App2() {
+  return(
+    <>
+      <h2>안녕하세요.</h2>
+    </>
+  )
+}
+
+export default App
