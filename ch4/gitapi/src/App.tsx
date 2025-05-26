c38cc6c050a1082cb3acf2e9a93e6023bfd1ac1a
maybeags <suren0709@gmail.com>
2025-05-26 14:50:48 +0900

    feat: useQuery lesson

--- ch4/gitapi/src/App.tsx
+++ ch4/gitapi/src/App.tsx
@@ -1,12 +1,17 @@
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Repositories from './components/Repositories';
 import './App.css'
 
 function App() {
+
+  const queryClient = new QueryClient();
   
 
   return (
     <>
-      
+      <QueryClientProvider client={queryClient}>
+        <Repositories />
+      </QueryClientProvider>
     </>
   )
 }
