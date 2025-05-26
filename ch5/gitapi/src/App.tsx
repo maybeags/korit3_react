c55d9f4ebb5407712821085e936e841e54636f67
maybeags <suren0709@gmail.com>
2025-05-26 15:38:53 +0900

    feat: gitapi copied

---
+++ ch5/gitapi/src/App.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Repositories from './components/Repositories';
+import './App.css'
+
+function App() {
+
+  const queryClient = new QueryClient();
+  
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Repositories />
+      </QueryClientProvider>
+    </>
+  )
+}
+
+export default App
