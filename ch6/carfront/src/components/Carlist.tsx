b3b44fd1aa9c7aee35c016e23bef124b9b0e7541
maybeags <suren0709@gmail.com>
2025-05-28 17:05:38 +0900

    feat: C part complete

--- ch6/carfront/src/components/Carlist.tsx
+++ ch6/carfront/src/components/Carlist.tsx
@@ -3,6 +3,7 @@ import { getCars, deleteCar } from "../api/carapi";
 import { DataGrid, GridColDef, GridCellParams } from "@mui/x-data-grid";
 import { Snackbar } from "@mui/material";
 import { useState } from "react";
+import AddCar from "./AddCar";
 
 export default function Carlist() {
   const queryClient = useQueryClient();
@@ -63,6 +64,7 @@ export default function Carlist() {
   else {
     return(
       <>
+        <AddCar />
         <DataGrid 
           rows={data}
           columns={columns}
